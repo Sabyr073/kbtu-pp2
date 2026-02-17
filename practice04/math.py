f04e9cce44fb49061df1b41f4c8857f99b2b2c21
sabyr <s_akhmetov@kbtu.kz>
2026-02-17 09:46:10 +0500

    Add python dates example

---
+++ practice04/math.py
@@ -0,0 +1,39 @@
+# 1
+# The min() and max() functions can be used to find the lowest or highest value in an iterable
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+# 2 
+# The abs() function returns the absolute (positive) value of the specified number
+x = abs(-7.25)
+
+print(x)
+
+# 3
+# The pow(x, y) function returns the value of x to the power of y
+# Return the value of 4 to the power of 3
+x = pow(4, 3)
+
+print(x)
+
+# 4
+# The math.sqrt() method for example, returns the square root of a number
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+# 5
+# math.ceil() method rounds a number upwards to its nearest integer
+# math.floor() method rounds a number downwards to its nearest integer
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
